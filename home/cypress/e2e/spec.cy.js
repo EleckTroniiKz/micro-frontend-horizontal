b01b435ba71f93fdc02f20ab9dc4c5c321e91be0
ucc <ucc@eschbach.com>
2023-10-01 11:27:36 +0200

    add new test

--- home/cypress/e2e/spec.cy.js
+++ home/cypress/e2e/spec.cy.js
@@ -17,4 +17,33 @@ describe("E2E Tests for the home application", () => {
     cy.contains("TestCountry").should("be.visible");
     cy.contains("TestHeight").should("be.visible");
   });
+
+  it("adds user, and deletes user from list", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("a:nth-child(3)").click();
+    cy.get('[data-testid="name"]').click();
+    cy.get('[data-testid="name"]').type("TestUserDelete");
+    cy.get('[data-testid="gender"]').click();
+    cy.get('[data-testid="gender"]').type("TestGenderDelete");
+    cy.get('[data-testid="country"]').click();
+    cy.get('[data-testid="country"]').type("TestCountryDelete");
+    cy.get('[data-testid="height"]').click();
+    cy.get('[data-testid="height"]').type("TestHeightDelete");
+    cy.get('[data-testid="addUser"]').click();
+    cy.reload();
+    cy.contains("TestUserDelete").should("be.visible");
+    cy.contains("TestGenderDelete").should("be.visible");
+    cy.contains("TestCountryDelete").should("be.visible");
+    cy.contains("TestHeightDelete").should("be.visible");
+
+    // now delete that user
+
+    const parent = cy.contains("td", "TestUserDelete").parent();
+    const deleteButton = parent.find("button").contains("Delete");
+    deleteButton.click();
+    cy.contains("td", "TestUserDelete").should("not.exist");
+    cy.contains("td", "TestGenderDelete").should("not.exist");
+    cy.contains("td", "TestCountryDelete").should("not.exist");
+    cy.contains("td", "TestHeightDelete").should("not.exist");
+  });
 });
